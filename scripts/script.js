67eb177f5ddf925d664555cf9e6c00dcb1dc0754
Stephane <stephanefokyin@gmail.com>
2022-08-25 10:28:25 +0400

    feat: further integration of challenge design

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,32 @@
-const hamburgerBtn = document.querySelector('.hamburger--icon');
+//
+//  dom element variables
+//
+
+const hamburgerBtn = document.querySelector('.hamburger--icon')
 const menu = document.querySelector('.menu')
 const backdrop = document.querySelector('.backdrop')
 const innerList = document.querySelector('.inner-list')
+const navInnerListFeatures = document.querySelector('.nav--inner-list__features')
+const navInnerListCompany = document.querySelector('.nav--inner-list__company')
 
+// Open menu arrows mobile screen 
 const featureArrow = document.querySelector('#feature-arrow')
 const companyArrow = document.querySelector('.company-arrow')
+
+// Open menu arrow navbar on desktop screen
+
+const featuresNav = document.querySelector('.nav--features')
+const companyNav = document.querySelector('.nav--company')
 
 const line1 = document.querySelector('.line1')
 const line2 = document.querySelector('.line2')
 const line3 = document.querySelector('.line3')
 
 
-
+//
 // event-listeners
+//
+
     // display slide menu
 hamburgerBtn.addEventListener('click', (e) => {
 
@@ -34,16 +48,43 @@ hamburgerBtn.addEventListener('click', (e) => {
     }
 })
 
-    // display dropdown menus
+    // display dropdown menus on mobile
 
-featureArrow.addEventListener('click', () => {
-
-    if (!innerList.classList.contains('expanded')) {
-        innerList.classList.remove('collapsed')
-        innerList.classList.add('expanded')
-    } else {
-        innerList.classList.add('collapsed')
-        innerList.classList.remove('expanded')
-    }
+featureArrow.addEventListener('click', function(e) {
+    toggleMenu(innerList)
 })
 
+
+    // display dropdown menus on desktop
+
+featuresNav.addEventListener('click', function(e) {
+    toggleMenu(navInnerListFeatures)
+})
+companyNav.addEventListener('click', function(e) {
+    toggleMenu(navInnerListCompany)
+})
+
+
+
+
+
+
+
+
+
+//
+// utility functions
+//
+
+function toggleMenu(element) {
+
+    console.log(element)
+
+    if (!element.classList.contains('expanded')) {
+        element.classList.remove('collapsed')
+        element.classList.add('expanded')
+    } else {
+        element.classList.add('collapsed')
+        element.classList.remove('expanded')
+    }
+}
