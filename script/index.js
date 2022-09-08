88dd1cb2e96114ba3ffff7b17ac026c258139943
Stephane <stephanefokyin@gmail.com>
2022-09-08 16:41:01 +0400

    feat further integration

--- script/index.js
+++ script/index.js
@@ -1,22 +1,24 @@
 // element selection
 const tabsList = document.querySelector('.tabs--selector__list');
-
+const faqQuestionsList = document.querySelectorAll('.faq--section__arrow');
 
 const bookmarkTab = document.querySelector('.bookmark--tab');
 const intelligentSearchTab = document.querySelector('.intelligent--search__tab');
 const shareBookmarsTab = document.querySelector('.share--bookmarks__tab');
 
 
-const selectorsArray = Array.from(tabsList.children);
-
+const tabsSelectorsArray = Array.from(tabsList.children);
+const faqQuestionsArray = Array.from(faqQuestionsList);
 
 
 // global state
 let activeTab = 0;
 
 
-// tab selection lisenter
-selectorsArray.forEach((element, index, arr) => {
+console.log(faqQuestionsArray);
+
+// tab selection listener
+tabsSelectorsArray.forEach((element, index, arr) => {
     element.addEventListener('click', () => {
 
         let activeTab = index
@@ -61,3 +63,72 @@ selectorsArray.forEach((element, index, arr) => {
 
     bookmarkTab.classList.add('active');
 })
+
+
+//faq questions listeners
+
+faqQuestionsArray.forEach((btn, index, arr) => {
+    btn.addEventListener("click", () => {
+
+
+        console.log(btn)
+
+        if (index === 0 ) {
+            openFaqAnswer(arr[0])
+
+            closeFaqAnswer(arr[1]);
+            closeFaqAnswer(arr[2]);
+            closeFaqAnswer(arr[3]);
+        }
+        if (index === 1 ) {
+            openFaqAnswer(arr[1])
+
+            closeFaqAnswer(arr[0]);
+            closeFaqAnswer(arr[2]);
+            closeFaqAnswer(arr[3]);
+        }
+        if (index === 2 ) {
+            openFaqAnswer(arr[2])
+
+            closeFaqAnswer(arr[0]);
+            closeFaqAnswer(arr[1]);
+            closeFaqAnswer(arr[3]);
+        }
+        if (index === 3 ) {
+            openFaqAnswer(arr[3])
+
+            closeFaqAnswer(arr[0]);
+            closeFaqAnswer(arr[1]);
+            closeFaqAnswer(arr[2]);
+        }
+       
+    })
+})
+
+
+
+// utils functions
+
+function openFaqAnswer(element) {
+    
+    if(element.parentNode.parentNode.classList.contains('active')) { 
+        closeFaqAnswer(element)
+    } else {
+        
+        element.parentNode.parentNode.classList.add('active');
+        
+        element.firstElementChild.setAttribute('transform', 'rotate(180, -5, -5)');
+        element.firstElementChild.firstChild.attributes.getNamedItem('stroke').value = 'hsl(0, 94%, 66%)';
+
+    }
+
+
+}
+
+function closeFaqAnswer(element) {
+
+    element.firstElementChild.setAttribute('transform', '');
+    element.firstElementChild.firstChild.attributes.getNamedItem('stroke').value = '#5267DF';
+    element.parentNode.parentNode.classList.remove('active');
+
+}
