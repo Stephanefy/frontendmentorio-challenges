f7e0afdb9f0eeb2a62643fa68042d836f7e4fad3
Stephanefy <stephanefokyin@gmail.com>
2022-12-05 14:34:55 +0100

    feat: fixed modal search filter asset import

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -15,7 +15,7 @@ function Modal() {
           <div className="bg-white opacity-100 w-5/6 h-40 fixed top-1/3 left-1/2 tansform -translate-x-1/2 z-50 rounded-md">
               <div className="flex items-center w-12/12 px-3 border-b border-gray-300">
                   <span className='px-2'>
-                      <img src="/src/assets/desktop/icon-location.svg" alt="search-icon" width={24} height={17}/>
+                      <img src="/assets/desktop/icon-location.svg" alt="search-icon" width={24} height={17}/>
                   </span>
                   <label className="hidden">search-term</label>
                   <input className="p-3 border-none w-full" type="text" id="role" name="role" placeholder="Filter by Location"/>
